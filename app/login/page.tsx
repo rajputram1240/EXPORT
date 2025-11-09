fc24e72178adc4559e810f8ea99a8be504820c80
rajputram1240 <pratap11191@gmail.com>
2025-11-10 01:21:59 +0530

    update code

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,4 +1,6 @@
 'use client';
+export const dynamic = 'force-dynamic';
+
 import { useState } from 'react';
 import { useSearchParams, useRouter } from 'next/navigation';
 import type { Route } from "next";
